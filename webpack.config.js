7723ce11df10b2cdbcba5836f44c74b86c4f7e37
Kira <priyanshushrama709@gmail.com>
2020-12-05 13:21:54 +0530

    add CSS and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,18 @@ module.exports = {
     // change to dev or prod mode
     mode: "development",
     // show code not in eval
-    devtool: "none",
+
+    // devtool: "none",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 }
